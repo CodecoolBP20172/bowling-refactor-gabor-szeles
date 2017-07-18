42f4dbf9c841b233f628973d87736f8f862fc8f6
gabor-szeles <szelesgbr@gmail.com>
2017-07-18 14:51:37 +0200

    function added to lessen complexity

--- bowling.py
+++ bowling.py
@@ -10,17 +10,12 @@ def score(game):
                 result += 10 - last
                 result += get_value(game[i+1])
             elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
+                result += if_is_X(game[i+1], game[i+2])
         last = get_value(game[i])
-        if not in_first_half:
-            frame += 1
         if in_first_half:
             in_first_half = False
         else:
+            frame += 1
             in_first_half = True
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
@@ -39,3 +34,12 @@ def get_value(char):
         return 0
     else:
         raise ValueError()
+
+
+def if_is_X(plus_one, plus_two):
+    result = get_value(plus_one)
+    if plus_two == '/':
+        result += 10 - get_value(plus_one)
+    else:
+        result += get_value(plus_two)
+    return result
